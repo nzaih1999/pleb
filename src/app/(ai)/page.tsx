456a9006121dc4eed18b18baa4f552ead95a8316
mundume <nzaih18@gmail.com>
2024-09-18 23:56:00 +0300

    sad nigga hours

--- src/app/(ai)/page.tsx
+++ src/app/(ai)/page.tsx
@@ -5,23 +5,24 @@ import Image from "next/image";
 import { useState } from "react";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Navbar from "@/components/navbar";
+import { Metadata } from "next";
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "RenderCon 2024 Badge",
+    description: "Preview of RenderCon 2024 Badge",
+    openGraph: {
+      title: "RenderCon 2024 Badge",
+      description: "Preview of RenderCon 2024 Badge",
+      images: [
+        {
+          url: `https://rendercon-24.vercel.app/opengraph-image?af80a6e56ddfdc86`,
+        },
+      ],
+    },
+  };
+}
 export default function Home() {
-  const placeholders = [
-    "What's the first rule of Fight Club?",
-    "Who is Tyler Durden?",
-    "Where is Andrew Laeddis Hiding?",
-    "Write a Javascript method to reverse a string",
-    "How to assemble your own PC?",
-  ];
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("submitted");
-  };
   return (
     <main className="min-h-screen h-full max-l mx-auto">
       <Navbar />
