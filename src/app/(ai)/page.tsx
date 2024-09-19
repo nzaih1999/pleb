9f87a074d7733b1b790b87f218feb1290936709e
mundume <nzaih18@gmail.com>
2024-09-19 10:55:35 +0300

    fix even more stuff

--- src/app/(ai)/page.tsx
+++ src/app/(ai)/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
+    metadataBase: new URL("https://rendercon-24.vercel.app"),
     title: "RenderCon 2024 Badge",
     description: "Preview of RenderCon 2024 Badge",
     openGraph: {
